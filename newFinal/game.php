8d2c9ec7d4313010ff8ef358f4afa16e633cfb9e
Josh Seifert <seiferjo@onid.oregonstate.edu>
2015-05-20 10:16:12 -0700

    In progress.

--- newFinal/game.php
+++ newFinal/game.php
@@ -9,6 +9,35 @@ function showTeams()
   } 	
 }
 
+function showGames()
+{
+  global $mysqli;
+  $temp = "SELECT id, home_team, away_team, home_score, away_score, date FROM game";
+  $rows = $mysqli->query($temp)->fetch_all();
+  echo '<form action="game.php" method="post" ><select name="game_id" required>';
+  foreach($rows as $value){
+    $tempHomeTeam = $mysqli->query("SELECT name FROM team WHERE id = '" . $value[1] . "'")->fetch_row()[0];
+    $tempAwayTeam = $mysqli->query("SELECT name FROM team WHERE id = '" . $value[2] . "'")->fetch_row()[0];
+    echo "<option value = '$value[0]'>$value[5]: $tempHomeTeam:$value[3], $tempAwayTeam:$value[4]</option>";
+  } 
+  echo '</select><input type="submit" value = "Submit"></input></form>';
+  
+
+/*PROBLEM IS HERE. Only want players who are playing in the current game. Find a way to uniquely identify the player when they submit info to PHP file, 
+to give them a score.*/  
+  if(isset($_POST['game_id']))
+  {
+    echo '<table border = 1> <tr> <td>First Name</td> <td>Last Name</td> <td>Team</td><td>Score</td></tr>';
+    $res = $mysqli->query("SELECT p.first_name, p.last_name, t.name FROM player p INNER JOIN team t ON t.id = p.team_id")->fetch_all();
+    for ($i = 0; $i < count($res); $i++){
+      echo "<tr><td>" . $res[$i][0] . "</td><td>" . $res[$i][1] . "</td><td>" . $res[$i][2] . "</td><td><input type='number' name='player_id' value='0' min='0' max='99'></td></tr>";
+    }
+    echo "</table>";
+  }	  
+	
+}
+
+
 function deleteGame()
 {
   global $mysqli;
@@ -54,7 +83,10 @@ function deleteGame()
 	Date*: <input type="date" name="date" value="2015-01-01" min="2000-01-01" max="2015-12-31"><br />
 	<p>* Denotes a required field.</p>
   <input type="submit">
-  </form>  
+  </form>
+  
+  <h4>Did your favorite player score? Log who scored, and in what games.</h4>
+    <?php showGames(); ?>
   
   <h4>Some games are so ugly, you want to pretend they never happened. Now you can! Get rid of evidence that your favorite team is a perennial disappointment.</h4>
   Select Game:
