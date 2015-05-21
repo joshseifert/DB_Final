ad1741d94d0d57697286a017f862dddbac2d112b
Josh Seifert <seiferjo@onid.oregonstate.edu>
2015-05-20 20:23:45 -0700

    Added "player-game" table. When adding game, specify who scored, saves in DB.

--- newFinal/game.php
+++ newFinal/game.php
@@ -1,7 +1,9 @@
 <?php
+include "userinfo.php";
+
 function showTeams()
 {
-  global $mysqli;
+  
   $temp = "SELECT name FROM team";
   $rows = $mysqli->query($temp)->fetch_all();
   foreach($rows as $value){
@@ -9,39 +11,10 @@ function showTeams()
   } 	
 }
 
-function showGames()
-{
-  global $mysqli;
-  $temp = "SELECT id, home_team, away_team, home_score, away_score, date FROM game";
-  $rows = $mysqli->query($temp)->fetch_all();
-  echo '<form action="game.php" method="post" ><select name="game_id" required>';
-  foreach($rows as $value){
-    $tempHomeTeam = $mysqli->query("SELECT name FROM team WHERE id = '" . $value[1] . "'")->fetch_row()[0];
-    $tempAwayTeam = $mysqli->query("SELECT name FROM team WHERE id = '" . $value[2] . "'")->fetch_row()[0];
-    echo "<option value = '$value[0]'>$value[5]: $tempHomeTeam:$value[3], $tempAwayTeam:$value[4]</option>";
-  } 
-  echo '</select><input type="submit" value = "Submit"></input></form>';
-  
-
-/*PROBLEM IS HERE. Only want players who are playing in the current game. Find a way to uniquely identify the player when they submit info to PHP file, 
-to give them a score.*/  
-  if(isset($_POST['game_id']))
-  {
-    echo '<table border = 1> <tr> <td>First Name</td> <td>Last Name</td> <td>Team</td><td>Score</td></tr>';
-    $res = $mysqli->query("SELECT p.first_name, p.last_name, t.name FROM player p INNER JOIN team t ON t.id = p.team_id")->fetch_all();
-    for ($i = 0; $i < count($res); $i++){
-      echo "<tr><td>" . $res[$i][0] . "</td><td>" . $res[$i][1] . "</td><td>" . $res[$i][2] . "</td><td><input type='number' name='player_id' value='0' min='0' max='99'></td></tr>";
-    }
-    echo "</table>";
-  }	  
-	
-}
-
-
 function deleteGame()
 {
   global $mysqli;
-  $temp = "SELECT id, home_team, away_team, home_score, away_score, date FROM game";
+  $temp = "SELECT id, home_team, away_team, home_score, away_score, date FROM game ORDER BY date ASC";
   $rows = $mysqli->query($temp)->fetch_all();
   echo '<form action="deleteGame.php" method="post" ><select name="game_id" required>';
   foreach($rows as $value){
@@ -52,6 +25,32 @@ function deleteGame()
   echo '</select><input type="submit" value = "Delete" onclick = "return confirmDelete()"></input></form>';
 	
 }
+
+function addGame()
+{
+  global $mysqli;
+  $temp = "SELECT name FROM team";
+  $rows = $mysqli->query($temp)->fetch_all();
+  
+	echo '<form action="game.php" method="post" >
+	  Home Team*: <select name="home_team" required>';
+    foreach($rows as $value){
+      echo "<option value = '$value[0]'>$value[0]</option>";
+    }
+    echo '</select><br />
+	
+	Away Team*: <select name="away_team" required>';
+    foreach($rows as $value){
+      echo "<option value = '$value[0]'>$value[0]</option>";
+    }
+  echo '</select><br />
+	Home Score*: <input type="number" name="home_score" value="0" min="0" max = "99" required><br />
+	Away Score*: <input type="number" name="away_score" value="0" min="0" max = "99" required><br />
+	Date*: <input type="date" name="date" value="2015-01-01" min="2000-01-01" max="2015-12-31"><br />
+	<p>* Denotes a required field.</p>
+  <input type="submit">
+  </form>';
+}
 ?>
 
 <!DOCTYPE HTML>
@@ -61,32 +60,54 @@ function deleteGame()
   <link rel="stylesheet" href="style.css">
   <script src="functions.js"></script>
   <meta charset='UTF-8'/>
-  <?php include "userinfo.php"?>
 </head>
 <body>
 <?php include "header.php"?>
 
   <h2>GAMES</h2>
   <h4>Record all the details of the latest match</h4>
-  
-  <form action="insertGame.php" method="post" >
-	<h4>ADD A GAME:</h4>
-	Home Team*: <select name="home_team" required>
-	  <?php showTeams(); ?>
-    </select><br />
-	Away Team*: <select name="away_team" required>
-	  <?php showTeams(); ?>
-    </select><br />
-	Home Score*: <input type="number" name="home_score" value="0" min="0" max = "99" required><br />
-	Away Score*: <input type="number" name="away_score" value="0" min="0" max = "99" required><br />
-    </select><br />
-	Date*: <input type="date" name="date" value="2015-01-01" min="2000-01-01" max="2015-12-31"><br />
-	<p>* Denotes a required field.</p>
-  <input type="submit">
-  </form>
-  
-  <h4>Did your favorite player score? Log who scored, and in what games.</h4>
-    <?php showGames(); ?>
+  <h4>ADD A GAME:</h4>
+ 
+<!--  <form action="insertGame.php" method="post" > -->
+	
+<!-- START OF PAGE HERE -->  
+  <?php 
+  if(!isset($_POST['home_team']))
+    addGame();
+    
+  if(isset($_POST['home_team']))
+  {
+	$home_id = $mysqli->query("SELECT id FROM team WHERE name = '" . $_POST['home_team'] . "'")->fetch_row()[0];
+	$away_id = $mysqli->query("SELECT id FROM team WHERE name = '" . $_POST['away_team'] . "'")->fetch_row()[0];
+
+	echo 'Home Team: ' . $_POST['home_team'] . '<br />
+	Away Team: ' . $_POST['away_team'] . '<br />
+	Home Score: ' . $_POST['home_score'] . '<br />
+	Away Score: ' . $_POST['away_score'];	
+	
+
+    echo '<form action="insertGame.php" method="post">';
+	echo '<table border = 1> <tr><td>First Name</td> <td>Last Name</td> <td>Position</td> <td>Team</td> <td>Score</td></tr>';
+	$temp = "SELECT p.id, p.first_name, p.last_name, p.position, t.name 
+	  FROM player p 
+	  INNER JOIN team t ON p.team_id = t.id 
+	  WHERE t.id IN ('$home_id','$away_id')  ";
+    $rows = $mysqli->query($temp)->fetch_all();
+    for ($i = 0; $i < count($rows); $i++){
+      echo "<tr><td>" . $rows[$i][1] . "</td><td>" . $rows[$i][2] . "</td><td>" . $rows[$i][3] . "</td><td>" . $rows[$i][4] . "</td>";
+	  echo "<td><input type='number' name=" . $rows[$i][0] . " value=0 min=0 max=99>"; //Set name equal to player's id.
+    }
+	//"Hidden" variables Re-POST previous information regarding teams, scores.
+    echo "</table>
+      <input type='hidden' name='home_team' value='" . $_POST['home_team'] . "'/>
+	  <input type='hidden' name='away_team' value='" . $_POST['away_team'] . "'/>
+	  <input type='hidden' name='home_score' value='" . $_POST['home_score'] . "'/>
+	  <input type='hidden' name='away_score' value='" . $_POST['away_score'] . "'/>
+	  <input type='hidden' name='date' value='" . $_POST['date'] . "'/>
+      <input type='submit'>
+      </form>";
+  }
+  ?>
   
   <h4>Some games are so ugly, you want to pretend they never happened. Now you can! Get rid of evidence that your favorite team is a perennial disappointment.</h4>
   Select Game:
--- newFinal/insertGame.php
+++ newFinal/insertGame.php
@@ -7,7 +7,10 @@ if(isset($_POST['home_team'])){
 	$home_score = $_POST['home_score'];
 	$away_score = $_POST['away_score'];
 	$date = $_POST['date'];
-
+	
+	
+	//Adds the game to the 'game' table
+	
 	$home_team_id = $mysqli->query("SELECT id FROM team WHERE name = '" . $home_team . "'")->fetch_row()[0];
 	$away_team_id = $mysqli->query("SELECT id FROM team WHERE name = '" . $away_team . "'")->fetch_row()[0];
 	
@@ -22,6 +25,7 @@ if(isset($_POST['home_team'])){
 	
 	if($home_team_id == $away_team_id)
 	{
+//		echo "<script type='text/javascript'>alert('" . $home_team_id . "," . $away_team_id . "');</script>";
 		echo "<script type='text/javascript'>alert('Teams can\'t play themselves! Choose different Home and Away teams.');</script>";
         echo '<script type="text/javascript">window.location="game.php"</script>';
         exit();
@@ -39,6 +43,37 @@ if(isset($_POST['home_team'])){
       }
       $stmt->close();
 	}
+	
+	
+	//If a player played in a game, says how many goals they scored that game. Stored in "player-game" table.
+/*	$i = 0;
+    foreach($_POST as $key=>$value) {
+	if(is_int($key) && $value > 0)
+    {
+	  echo "i: '$i'   $key:   $value  <br />";
+	  $i += 1;
+	}*/
+	
+	//We want the ID of the most recently added game. Since it auto-increments, it will be the largest ID in the table.
+	$game_id = $mysqli->query("SELECT MAX(id) FROM game")->fetch_row()[0];
+
+    foreach($_POST as $key=>$value) {
+	if(is_int($key))
+    {
+	  if(!($stmt = $mysqli->prepare("INSERT INTO player_game (player_id, game_id, goals) VALUES (?, ?, ?)"))){
+        echo "Error preparing the statement.";
+      }
+      if(!($stmt->bind_param('iii', $key, $game_id, $value))){
+        echo "key: " . $key . "game_id:" . $game_id . "value: " . $value;
+      }
+      if(!($stmt->execute())){
+        echo "key: " . $key . "game_id:" . $game_id . "value: " . $value . "<br />";
+      }
+      $stmt->close();
+	}
+	
+	
+}
 
 header("location: game.php");
 exit();
